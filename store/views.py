66bb87ef14d2b5399f443d455d4fa5bb7f0a5c70
BenjaminDusse <78902387+BenjaminDusse@users.noreply.github.com>
2022-11-05 13:27:31 +0500

    created url to get products for taken collection_id

--- store/views.py
+++ store/views.py
@@ -7,8 +7,15 @@ from store.models import *
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get("collection_id")
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+
+        return queryset
 
     def get_serializer_context(self):
         return {"request": self.request}
@@ -34,11 +41,12 @@ class CollectionViewSet(ModelViewSet):
 
         return super().destroy(request, *args, **kwargs)
 
+
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
 
     def get_serializer_context(self):
-        return {'product_id': self.kwargs['product_pk']}
+        return {"product_id": self.kwargs["product_pk"]}
 
     def get_queryset(self):
-            return Review.objects.filter(product_id=self.kwargs['product_pk'])
+        return Review.objects.filter(product_id=self.kwargs["product_pk"])
