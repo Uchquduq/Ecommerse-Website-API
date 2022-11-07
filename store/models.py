926a7a216486ec1231f6fd75af2dd4cf34f7da2f
Benjamin Dusse <fazliddinabduhakimov9@gmail.com>
2022-11-07 23:11:24 +0500

    edited req.txt

--- store/models.py
+++ store/models.py
@@ -20,7 +20,6 @@ class Collection(models.Model):
     def __str__(self):
         return self.title
 
-
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
