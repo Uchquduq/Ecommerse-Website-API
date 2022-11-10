6c15f322fd8543c54e483e3ba955a04f1649c7b8
BenjaminDusse <78902387+BenjaminDusse@users.noreply.github.com>
2022-11-10 22:37:19 +0500

    created for order with items

--- store/admin.py
+++ store/admin.py
@@ -1,26 +1,57 @@
-from django.contrib import admin
-from store import models
-from django.urls import reverse
+from django.contrib import admin, messages
 from django.db.models.aggregates import Count
+from django.urls import reverse
 from django.utils.html import format_html, urlencode
+from django.db.models.query import QuerySet
+from store import models
 
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            queryset.filter(inventory__lt=10)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory', 'collection_title']
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
+    search_fields = ['title']
 
     def collection_title(self, product):
         return product.collection.title
 
-    # @admin.display(ordering='inventory')
-    # def inventory_status(self, product):
-    #     if product.inventory < 10:
-    #         return 'Low'
-    #     return 'OK'
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return product.inventory
         
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f"{updated_count} products were successfully updated.",
+            messages.SUCCESS
+        )
+
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -28,7 +59,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['user__first_name', 'user__last_name']
-    search_fields = ['first_name', 'last_name']
+    search_fields = ['user__first_name__istartswith', 'user__last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders_count(self, customer):
@@ -51,6 +82,7 @@ admin.site.register(models.Address)
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
@@ -68,6 +100,15 @@ class CollectionAdmin(admin.ModelAdmin):
             products_count = Count('product')
         )
 
+class OrderItemInline(admin.StackedInline):
+    autocomplete_fields = ['product']
+    model = models.OrderItem
+    extra = 0
+    min_num = 1
+    max_num = 10
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'customer']
+    inlines = [OrderItemInline]
+    autocomplete_fields = ['customer']
