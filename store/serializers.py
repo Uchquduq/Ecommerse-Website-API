cabbbd846c93f1a34eb8fe5889a981f457d83b68
Benjamin Dusse <fazliddinabduhakimov9@gmail.com>
2022-11-13 06:53:31 -0800

    query for when customer creates order create together and creates order items

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from django.db import transaction
 from django.db.models.aggregates import Count
 from rest_framework import serializers
 from store.models import *
@@ -70,7 +71,7 @@ class CartItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CartItem
-        fields = ["id", "product", "quantity", 'total_price']
+        fields = ["id", "product", "quantity", "total_price"]
 
 
 class CartSerializer(serializers.ModelSerializer):
@@ -79,11 +80,13 @@ class CartSerializer(serializers.ModelSerializer):
     total_price = serializers.SerializerMethodField()
 
     def get_total_price(self, cart):
-        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+        return sum(
+            [item.quantity * item.product.unit_price for item in cart.items.all()]
+        )
 
     class Meta:
         model = Cart
-        fields = ["id", "items", 'total_price']
+        fields = ["id", "items", "total_price"]
 
 
 class AddCartItemSerializer(serializers.ModelSerializer):
@@ -91,17 +94,17 @@ class AddCartItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CartItem
-        fields = ['id', 'product_id', 'quantity']
+        fields = ["id", "product_id", "quantity"]
 
     def validate_product_id(self, value):
         if not Product.objects.filter(pk=value).exists():
-            raise serializers.ValidationError('No product with given ID')
-        return value  
+            raise serializers.ValidationError("No product with given ID")
+        return value
 
     def save(self, **kwargs):
-        cart_id = self.context['cart_id']
-        product_id = self.validated_data['product_id']
-        quantity = self.validated_data['quantity']
+        cart_id = self.context["cart_id"]
+        product_id = self.validated_data["product_id"]
+        quantity = self.validated_data["quantity"]
 
         try:
             # Update an existing item
@@ -111,23 +114,25 @@ class AddCartItemSerializer(serializers.ModelSerializer):
             self.instance = cart_item
         except CartItem.DoesNotExist:
             # Create a new item
-            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data) # hammasini bir-biriga tenglashtirmasdan qolganlarini shunaqa qilsa boladi
+            self.instance = CartItem.objects.create(
+                cart_id=cart_id, **self.validated_data
+            )  # hammasini bir-biriga tenglashtirmasdan qolganlarini shunaqa qilsa boladi
 
         return self.instance
 
-class UpdateCartItemSerializer(serializers.ModelSerializer):
 
+class UpdateCartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
-        fields = ['quantity']
-        
+        fields = ["quantity"]
+
+
 class CustomerSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(read_only=True)
-    
+
     class Meta:
         model = Customer
-        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
-
+        fields = ["id", "user_id", "phone", "birth_date", "membership"]
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -136,26 +141,46 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = OrderItem
-        fields = ['id', 'product', 'unit_price', 'quantity']
+        fields = ["id", "product", "unit_price", "quantity"]
+
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
 
     class Meta:
         model = Order
-        fields = ['id', 'customer', 'placed_at', 'payment_status', 'items']
+        fields = ["id", "customer", "placed_at", "payment_status", "items"]
+
 
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        print(dir(self.validated_data))
-        print(self.context['user_id'])
+        # print(self.validated_data["cart_id"])
+        # print(dir(self.validated_data))
+        # print(self.context["user_id"])
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
+            (customer, created) = Customer.objects.get_or_create(
+                user_id=self.context["user_id"]
+            )
+            print(customer)
+            order = Order.objects.create(customer=customer)
+            cart_items = CartItem.objects \
+                            .select_related('product') \
+                            .filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order = order,
+                    product = item.product,
+                    unit_price = item.product.unit_price,
+                    quantity = item.quantity
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
 
-        (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
-        print(customer)
-        Order.objects.create(customer=customer)
+            Cart.objects.filter(pk=cart_id).delete()
+
+
 
         # tepadagi kodda qachon customer yaratilsa u bilan birgalikda bitta vaqtda customer ham yaratiladi.
-
